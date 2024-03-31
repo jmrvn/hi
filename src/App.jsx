23a2a1ca186b4e80ac71af53e10675850bda8b37
wei <nuquejmrvn@gmail.com>
2024-03-31 13:44:25 +0800

    kitchie

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import React from 'react';
+import sound from "./assets/kitchie.mp3";
 
 function App() {
+  function play() {
+    new Audio(sound).play();
+  }
+
   return (
-    <div className="min-h-screen flex justify-center items-center">
-    <h1 className="text-orange-50 text-7xl font-bold text-center">HELLO WORLD TEST</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-orange-50 text-4xl font-bold text-center m-4">HELLO!</h1>
+      <button className="bg-slate-200 font-bold w-52 h-10 rounded-lg" onClick={play}>
+        CLICK ME
+      </button>
     </div>
   );
 }
