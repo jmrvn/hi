946f683f436624bdca08921c59895b4db17eacaf
wei <nuquejmrvn@gmail.com>
2024-03-31 12:11:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+    <h1 className="text-orange-50 text-7xl font-bold text-center">GUSTO KITA</h1>
+    </div>
+  );
+}
+
+export default App;
